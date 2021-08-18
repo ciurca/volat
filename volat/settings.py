a6b04c1c243a4b26102ef47bd3ffb92d9aa1a4a2
ciurca <raduciurca@gmail.com>
2021-08-18 19:32:09 +0300

    DEBUG FALSE

--- volat/settings.py
+++ volat/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["volat-hrm.herokuapp.com", "127.0.0.1", "volat.xyz", "www.volat.xyz"]
 
