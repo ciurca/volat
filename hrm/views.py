3f07cb4a52807fd3c85662c3e2c83eae5a79201c
ciurca <raduciurca@gmail.com>
2021-08-18 21:03:45 +0300

    removed lower

--- hrm/views.py
+++ hrm/views.py
@@ -78,7 +78,7 @@ def loginPage(request):
 		username = request.POST.get('username')
 		password = request.POST.get('password')
 
-		user = authenticate(request, username = username.lower(), password = password)
+		user = authenticate(request, username = username, password = password)
 		if user is not None:
 			login(request, user)
 			return redirect('home')
